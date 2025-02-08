58b5988cb5c2b2958c21e248f43f28c9ab42a614
Daniel Soden <dsoden09@gmail.com>
2025-02-08 18:10:38 +0000

    added startup project

--- goedit.go
+++ goedit.go
@@ -1,9 +1,25 @@
-package goedit
+package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
+func displayFileContents(fileName string) string{
+	file, err := os.ReadFile(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(file)
+}
+
+func newValues(){
+
+}
+
 func main() {
-	fmt.Println("Hi there")
+	fmt.Println("Welcome to dedit")
+	displayFileContents("test.txt")
 }
